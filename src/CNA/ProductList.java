12253de7a16e3c6fc995f21b2c84efa85c06adf0
Shaamala <shaamalajabbar@gmail.com>
2018-02-08 13:34:13 -0500

    add new constructor and modify toString output format

--- src/CNA/ProductList.java
+++ src/CNA/ProductList.java
@@ -1,15 +1,14 @@
 package CNA;
 
-public class ProductList extends Product{
-int num;
-    public ProductList(){
+public class ProductList extends Product {
+    int num;
+
+    public ProductList() {
 
     }
-    public ProductList(String name, double price){
 
-        super(name,price);
-    }
-    public ProductList(int num, String name, double price, String category, String description){
+
+    public ProductList(int num, String name, double price, String category, String description) {
 
         super(name, price, category, description);
         this.num = num;
@@ -19,13 +18,18 @@ int num;
         return num;
     }
 
+    public ProductList(int num, String name, double price) {
+
+        super(name, price);
+        this.num = num;
+    }
     public void setNum(int num) {
         this.num = num;
     }
 
     @Override
     public String toString() {
-        return num + "- " + super.getName()+"   "+super.getPrice()+"\nCategory: "+super.getCategory()+"\n"+super.getDescription();
+        return num + "- " + super.getName() + "\t\t\t\t\t\t" + super.getPrice() + "\n\t Category: " + super.getCategory() + "\n\t " + super.getDescription();
     }
 
 }
